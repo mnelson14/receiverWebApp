99c7393e6f30488864a2bf39212ae1195f5efb35
Mike N <34198770+mnelson14@users.noreply.github.com>
2021-07-04 14:01:14 -0700

    Update receiver.js

--- receiver.js
+++ receiver.js
@@ -1,8 +1,16 @@
 
 const context = cast.framework.CastReceiverContext.getInstance();
+context.setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
+const playerManager = context.getPlayerManager();
 
-// Update style using javascript
-// let playerElement = document.getElementsByTagName("cast-media-player")[0];
-// playerElement.style.setProperty('--splash-image', 'url("http://some/other/image.png")');
+playerManager.addEventListener(
+    cast.framework.events.EventType.ERROR, (event) => {
+      // Write your own event handling code, for example
+      // using the event.mediaStatus value
+
+      // Update style using javascript
+      let playerElement = document.getElementsByTagName("cast-media-player")[0];
+      playerElement.style.setProperty('--splash-image', 'url("getApp.png")');
+});
 
 context.start();
